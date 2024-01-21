757acf632cb204009d79a45c546b84d7c1446a1e
Varune Tomar <varunetomar@gmail.com>
2024-01-21 17:49:32 -0500

    firebasegooglev1

--- index.js
+++ index.js
@@ -1,93 +1,51 @@
 console.log("main process working");
 
-console.log('main.js');
-
-
-
-const electron =  require("electron");
-
-const app = electron.app;
-
-const BrowserWindow = electron.BrowserWindow;
-
+const electron = require("electron");
+const express = require('express');
 const path = require("path");
-
 const url = require("url");
 
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
+const appServer = express();
+const serverPort = 3000;
+
+appServer.use(express.static(path.join(__dirname, 'public')));
+
+appServer.listen(serverPort, () => {
+  console.log(`Server is running on http://localhost:${serverPort}`);
+});
 
 let win;
 
+function createWindow() {
+  win = new BrowserWindow({
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        webSecurity: false, 
+        allowRunningInsecureContent: true,
+    }
+  });
 
-function createWindow(){ //creates window
+  win.loadURL(`http://localhost:${serverPort}/signUp.html`);
 
-    win = new BrowserWindow({
-
-        webPreferences:{
-
-            nodeIntegration:true,
-
-            contextIsolation:false,
-
-        }
-
-    });
-
-
-
-    win.loadURL(url.format({ //loads signup page
-
-        pathname: path.join(__dirname, 'signUp.html'),
-
-        protocol: 'file',
-
-        slashes: true
-
-    }));
-
-
-
-    
-
-    // win.webContents.openDevTools(); //just here to check for some stuff, remove later
-
-
-    
-
-
-
-    win.on('closed', ()=>{
-
-        win = null;
-
-    })
-
+  win.on('closed', () => {
+    win = null;
+  });
 }
-
-
 
 app.on('ready', createWindow);
 
-
-
-app.on('window-all-closed', () =>{
-
-    if(process.platform !== 'darwin'){
-
-        app.quit()
-
-    }
-
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-
-
-app.on('activate', ()=> {
-
-    if(win === null){
-
-        createWindow()
-
-    }
-
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
 });
---
+++ public/firebase.js
@@ -0,0 +1,11 @@
+// firebase.js
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAuj-AVSSgdL9QKDvCr6C4WBfb_o_RhiR8",
+  authDomain: "mon-e-2dbd6.firebaseapp.com",
+  projectId: "mon-e-2dbd6",
+  storageBucket: "mon-e-2dbd6.appspot.com",
+  messagingSenderId: "983558902043",
+  appId: "1:983558902043:web:93b3e671eeafa0a99503be"
+};
