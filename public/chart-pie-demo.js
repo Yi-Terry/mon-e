1f3be10e4cb6e57699c10564bd07c5a9eefcde19
ngoandreww <124756528+ngoandreww@users.noreply.github.com>
2024-04-04 14:44:18 -0400

    labels shown for chart, responsive

--- public/chart-pie-demo.js
+++ public/chart-pie-demo.js
@@ -2,16 +2,24 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
+
+
+// Sample data array
+var data = [5005, 300, 1105, 2000, 400];
+
+// Calculate total amount
+var total = data.reduce((acc, cur) => acc + cur, 0);
+
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Direct", "Referral", "Social", "Health Care", "Rent"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#87e76d', '#008000', '#1cc88a'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: data,
+      backgroundColor: ['#87e76d', '#008000', '#1cc88a','#FDA403','#E84545'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf','#2c9faf', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
@@ -28,41 +36,62 @@ var myPieChart = new Chart(ctx, {
       caretPadding: 10,
     },
     legend: {
-      display: false
+      display: true,
+      position: 'right',
+      labels: {
+        fontSize: 14,
+        fontColor: '#333',
+        usePointStyle: true,
+        generateLabels: function(chart) {
+          var data = chart.data;
+          if (data.labels.length && data.datasets.length) {
+            return data.labels.map(function(label, i) {
+              var meta = chart.getDatasetMeta(0);
+              var ds = data.datasets[0];
+              var arc = meta.data[i];
+              var custom = arc && arc.custom || {};
+              var getValueAtIndexOrDefault = Chart.helpers.getValueAtIndexOrDefault;
+              var arcOpts = chart.options.elements.arc;
+              var fill = custom.backgroundColor ? custom.backgroundColor : getValueAtIndexOrDefault(ds.backgroundColor, i, arcOpts.backgroundColor);
+              var stroke = custom.borderColor ? custom.borderColor : getValueAtIndexOrDefault(ds.borderColor, i, arcOpts.borderColor);
+              var bw = custom.borderWidth ? custom.borderWidth : getValueAtIndexOrDefault(ds.borderWidth, i, arcOpts.borderWidth);
+              return {
+                text: label + ': $' + ds.data[i],
+                fillStyle: fill,
+                strokeStyle: stroke,
+                lineWidth: bw,
+                hidden: isNaN(ds.data[i]) || meta.data[i].hidden,
+                index: i
+              };
+            });
+          }
+          return [];
+        }
+      }
     },
     cutoutPercentage: 80,
+
+
+
     plugins: {
-          doughnutlabel: {
-            labels: [
-              {
-                text: 'The title',
-                font: {
-                  size: '60'
-                }
-              },
-              {
-                text: 'The subtitle',
-                font: {
-                  size: '50'
-                },
-                color: 'grey'
-              },
-              {
-                text: '$100.000',
-                font: {
-                  size: '30'
-                },
-                color: 'red'
-              },
-              {
-                text: '95%',
-                font: {
-                  size: '45'
-                },
-                color: 'green'
-              }
-            ]
+      doughnutlabel: {
+        labels: [
+          {
+            text: '$' + total,
+            font: {
+              size: '30'
+            },
+            color: 'red'
+          },
+          {
+            text: 'Total',
+            font:{
+              size: '15'
+            }
           }
-        }
+        ]
+      }
+    }
+    
   },
 });
